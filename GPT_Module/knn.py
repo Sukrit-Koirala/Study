bdc278b371b37817b72774aade0612e68fcaee7c
Koirala <sukrit.koirala@SlurmLogin-IA-01.dsu.local>
2026-09-08 16:47:50 -0500

    Results (excluding generated cache files)

--- GPT_Module/knn.py
+++ GPT_Module/knn.py
@@ -12,6 +12,12 @@ def query_knn(index, values, query_keys, k):
     retrieved_values = values[neighbor_idx]
     return distances, retrieved_values
 
+# GPU supported query
+def query_knn_indices(index, query_keys, k):
+    """ query_keys: [m,D] Returns distances [M,k] and raw neightbour indices"""
+    distances, neighbor_idx = index.kneighbors(query_keys, n_neighbors=k)
+    return distances, neighbor_idx
+
 def softmax_np(x, axis=-1):
     x = x - x.max(axis=axis, keepdims=True)
     e = np.exp(x)
